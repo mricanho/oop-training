ca88050a56a0c0be7bf93e8e926ae295d7c7dc00
Miguel Ricaño <mricanho@gmail.com>
2021-09-06 13:36:39 -0500

    Add linters config

--- lib/student.rb
+++ lib/student.rb
@@ -1,7 +1,8 @@
-require './person.rb'
+# rubocop:disable Style/OptionalBooleanParameter
+require './person'
 
 class Student < Person
-  def initialize(age, classroom, name = "Unknown", parent_permission = true)
+  def initialize(age, classroom, name = 'Unknown', parent_permission = true)
     super(age, name, parent_permission)
     @classroom = classroom
   end
@@ -10,3 +11,4 @@ class Student < Person
     "¯\(ツ)/¯"
   end
 end
+# rubocop:enable Style/OptionalBooleanParameter
