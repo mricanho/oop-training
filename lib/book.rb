074bc1d321b28469611aca955275546ddb66e9f8
Miguel Ricaño <mricanho@gmail.com>
2021-09-06 17:33:05 -0500

    Add associations between rental and book

--- lib/book.rb
+++ lib/book.rb
@@ -1,8 +1,16 @@
+require './rental'
+
 class Book
-  attr_accessor :title, :author
+  attr_accessor :title, :author, :rentals
 
   def initialize(title, author)
     @title = title
     @author = author
+    @rentals = []
+  end
+
+  def add_rental(person, date)
+    Rental.new(date, seld, person)
+    @rentals.push(rental)
   end
 end
--- lib/rental.rb
+++ lib/rental.rb
@@ -1,7 +1,10 @@
 class Rental
-  attr_accessor :date
+  attr_accessor :date, :book, :person
 
   def initialize(date)
     @date = date
+    @book = book
+    book.rentals << self
+    person.rentals << self
   end
 end
