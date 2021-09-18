8d28002306203f03312721faa9c0456f2011761a
Dipesh Kumar <deepesh.97kumar@gmail.com>
2021-09-19 04:09:03 +0530

    Refactor book class

--- lib/book.rb
+++ lib/book.rb
@@ -13,4 +13,24 @@ class Book
     Rental.new(date, seld, person)
     @rentals.push(rental)
   end
+  
+  def self.create_new_book
+    puts 'Enter title: '
+    title = gets.chomp
+
+    puts 'Enter author: '
+    author = gets.chomp
+
+    Book.new(title, author)
+
+    puts 'Book creation success!'
+  end
+
+  def self.display_books(books)
+    if books.length.positive?
+      books.each { |book| puts "Title: #{book.title}, Author: #{book.author}" }
+    else
+      puts 'Books empty!!!'
+    end
+  end
 end
