e333ce80c0ada9404c5d25817c3c27ab5ffc7000
Dipesh Kumar <deepesh.97kumar@gmail.com>
2021-09-19 04:44:13 +0530

    Fix rubocop issue

--- lib/student.rb
+++ lib/student.rb
@@ -4,7 +4,7 @@ require_relative './person'
 class Student < Person
   attr_reader :classroom
 
-  def initialize(age, name = 'Unknown', parent_permission = true, classroom)
+  def initialize(age, classroom, name = 'Unknown', parent_permission = true,)
     super(age, name, parent_permission)
     @classroom = classroom
   end
