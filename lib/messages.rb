4bff1d92d06a167b4231ac0a40e1d1ec39ff51eb
Dipesh Kumar <deepesh.97kumar@gmail.com>
2021-09-19 04:31:14 +0530

    Fix message.rb

--- lib/messages.rb
+++ lib/messages.rb
@@ -1,5 +1,5 @@
 module Messages
-    INTRODUCTION = "\t\t\t\t\"WELCOME TO OOP TRAINING\".
-      \t\t\t\t\===================
-      Please follow all of the instructions.".freeze
-  end
+  INTRODUCTION = "\t\t\t\t\"WELCOME TO OOP TRAINING\".
+    \t\t\t\t\===================
+    Please follow all of the instructions.".freeze
+end
