ea8c0c4fac26b67102a5ba72be6fc9d4b4cd7cd2
Dipesh Kumar <deepesh.97kumar@gmail.com>
2021-09-19 04:08:21 +0530

    Refactor person

--- lib/person.rb
+++ lib/person.rb
@@ -22,6 +22,53 @@ class Person
     @name = @corrector.correct_name(@name)
   end
   
+  def self.display_persons(people)
+    if people.length.positive?
+      people.each { |people| puts "Name: #{people.name},  ID: #{people.id}, Age: #{people.age}" }
+    else
+      puts 'Students empty!!!'
+    end
+  end
+
+  def create_new_person
+    puts 'To create a new student enter (1), to create a new teacher enter (2)'
+    inputed_choice = gets.chomp
+
+    case inputed_choice
+    when '1'
+      puts 'Enter age: '
+      age = gets.chomp
+
+      puts 'Enter name: '
+      name = gets.chomp
+
+      puts 'Parent permission? (Enter Y for Yes or any other key for No)'
+      parent_permission = gets.chomp
+      parent_permission_ = parent_permission.downcase == 'y' ? true : false
+      Student.new(age, name, parent_permission_)
+
+      puts 'Student creation success!'
+      
+    when '2'
+      puts 'Enter age: '
+      age = gets.chomp
+
+      puts 'Enter name: '
+      name = gets.chomp
+
+      puts 'Enter specialization: '
+      specialization = gets.chomp
+
+      Teacher.new(age, specialization, name)
+
+      puts 'Teacher creation success!'
+
+    else
+      puts 'Invalid choice'
+      nil
+    end
+  end
+  
   def is_of_age?
     @age >= 18
   end
